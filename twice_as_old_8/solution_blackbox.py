904e8b1b8386895d75231884c9b08a6fe15419b1
Edvard Student <student.pn@ucu.edu.ua>
2024-04-11 14:36:17 +0300

    Addeed a link to the solution from blackbox

--- twice_as_old_8/solution_blackbox.py
+++ twice_as_old_8/solution_blackbox.py
@@ -1,6 +1,18 @@
 def twice_as_old(dad_years_old, son_years_old):
-    if dad_years_old < son_years_old:
-        return (son_years_old * 2) - dad_years_old
+    # Calculate the difference in ages
+    age_difference = dad_years_old - son_years_old
+
+    # If the father is currently twice as old as his son,
+    # then the difference in ages is equal to the son's age
+    if age_difference == son_years_old:
+        return 0
+
+    # If the father is currently more than twice as old as his son,
+    # then we need to find out how many years ago the father was twice as old
+    elif age_difference > son_years_old:
+        return age_difference - son_years_old
+
+    # If the father is currently less than twice as old as his son,
+    # then we need to find out how many years in the future the father will be twice as old
     else:
-        result = dad_years_old - (son_years_old * 2)
-        return abs(result) if result < 0 else result
+        return son_years_old - age_difference
